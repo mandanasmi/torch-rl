684191202592f1d84b1c314e1d3a5ddef2894d83
roggirg <roger.girgis@gmail.com>
2019-06-28 19:18:23 -0400

    Fixed issues in plotting script.

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -2,11 +2,28 @@ import matplotlib.pyplot as plt
 import csv
 import numpy as np
 import os
+import utils
+import argparse
 
-dir = "storage/HyruleDQN_seed_1_denser/"
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("--env", required=True,
+                    help="name of the environment to train on (REQUIRED)")
+parser.add_argument("--model", default=None,
+                    help="name of the model (default: {ENV}_{ALGO}_{TIME})")
+parser.add_argument("--seed", type=int, required=True,
+                    help="random seed (default: 1)")
+parser.add_argument("--dense-reward", action="store_true", default=False,
+                    help="Use dense reward during training.")
+args = parser.parse_args()
+
+# Get model directory
+model_dir = "storage/" + args.model +"_seed_"+str(args.seed)
+if args.dense_reward: model_dir += "_denser"
+utils.create_folders_if_necessary(model_dir)
 
 def prep_list(filename):
-    with open(dir+filename, 'r') as f:
+    with open(model_dir+filename, 'r') as f:
         reader = csv.reader(f)
         your_list = list(reader)
 
@@ -21,32 +38,32 @@ def prep_list(filename):
     return new_list
 
 
-if not os.path.isdir(dir+"plots/"):
-    os.mkdir(dir+"plots/")
-reward_file = "rewards.csv"
-success_file = "episode_success.csv"
-loss_file = "losses.csv"
+if not os.path.isdir(model_dir+"/plots/"):
+    os.mkdir(model_dir+"/plots/")
+reward_file = "/rewards.csv"
+success_file = "/episode_success.csv"
+loss_file = "/losses.csv"
 
 fig = plt.figure()
 plt.plot(prep_list(reward_file))
 fig.suptitle('Sum of Rewards', fontsize=20)
-plt.xlabel('timesteps (x100)', fontsize=16)
+plt.xlabel('trajectories (x100)', fontsize=16)
 plt.ylabel('Reward', fontsize=16)
-fig.savefig(dir+'plots/sum_rewards.jpg')
+fig.savefig(model_dir+'/plots/sum_rewards.jpg')
 
 fig = plt.figure()
 plt.plot(prep_list(success_file))
 fig.suptitle('Success Rate', fontsize=20)
-plt.xlabel('timesteps (/100)', fontsize=16)
+plt.xlabel('trajectories (x100)', fontsize=16)
 plt.ylabel('Average final reward', fontsize=16)
-fig.savefig(dir+'plots/success_rate.jpg')
+fig.savefig(model_dir+'/plots/success_rate.jpg')
 
 fig = plt.figure()
 plt.plot(prep_list(loss_file))
 fig.suptitle('Q-Value difference', fontsize=20)
-plt.xlabel('timesteps (/100)', fontsize=16)
+plt.xlabel('trajectories (x100)', fontsize=16)
 plt.ylabel('Loss', fontsize=16)
-fig.savefig(dir+'plots/losses.jpg')
+fig.savefig(model_dir+'/plots/losses.jpg')
 
 # orig_obs_file = "orig_obs.npy"
 # orig_obs = np.load(dir+orig_obs_file)
